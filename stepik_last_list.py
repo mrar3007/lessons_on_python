60404482ecfc8f27287a3f4d6bf1377216c223d6
Arkadiy Petrov <arkadiy271020@gmail.com>
2020-12-26 00:03:38 +0500

    stepik function

--- stepik_last_list.py
+++ stepik_last_list.py
@@ -1,9 +1,18 @@
-def f(x):
-    if x <= -2:
-        return 1 - (x + 2)**2
-    elif -2 < x <= 2:
-        return -(x/2)
-    else:
-        return (x - 2)**2 + 1
+def modify_list(lst):
+    i = 0
+    l = len(lst)
+    lst.sort()
+    while i < l:
+        x = lst[i]
+        if x % 2 != 0:
+            del lst[i]
+            l -= 1
+        else:
+            lst[i] = x // 2
+            i += 1
 
-print(f(-4.5))
+lst = [1, 2, 3, 4, 5, 6]
+modify_list(lst)
+print(lst)
+modify_list(lst)
+print(lst)
