0856f488174f29e4139b215a63e365cd11f0d1bd
Arkadiy Petrov <arkadiy271020@gmail.com>
2020-12-29 23:38:36 +0500

    stepik def

--- codewars/z2912.py
+++ codewars/z2912.py
@@ -1,4 +1,4 @@
-def closest_mod_5(x):
+'''def closest_mod_5(x):
     y = x
     if y % 5 == 0:
         return y
@@ -7,4 +7,16 @@ def closest_mod_5(x):
     return y
 
 print(closest_mod_5(5))
-print(closest_mod_5(7))
+print(closest_mod_5(7))'''
+
+def s(a, *vs, b=10):
+   res = a + b
+   for v in vs:
+       res += v
+   return res
+
+print(s(11, 10))
+s(11, b=20)
+s(5, 5, 5, 5, 1)
+s(21)
+s(11, 10)
