14ab9d968fc6dbfaeae73f755d5034cc372f71b0
Arkadiy Petrov <arkadiy271020@gmail.com>
2020-12-29 23:42:31 +0500

    stepik fiban

--- codewars/z2912.py
+++ codewars/z2912.py
@@ -8,7 +8,7 @@
 
 print(closest_mod_5(5))
 print(closest_mod_5(7))'''
-
+'''
 def s(a, *vs, b=10):
    res = a + b
    for v in vs:
@@ -20,3 +20,12 @@ s(11, b=20)
 s(5, 5, 5, 5, 1)
 s(21)
 s(11, 10)
+'''
+
+def fib(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1)+fib(x-2)
+
+print(fib(2))
