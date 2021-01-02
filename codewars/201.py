2f4c06f23e6d29fdc340500d20d7287c2fa82358
Arkadiy Petrov <arkadiy271020@gmail.com>
2021-01-02 20:49:05 +0500

    cdwr cashier

--- codewars/201.py
+++ codewars/201.py
@@ -15,6 +15,31 @@ print([1, 2, 'a', 'b'])'''
 
 print(longest("aretheyhere", "yestheyarehere"))
 '''
+
 '''def count_smileys(arr):
     return sum([1 for i in arr if (i[0] == ':' or i[0] == ';') and (i[1] == i[len(i)-1] or i[1] == '~' or i[1] == '-') and (i[len(i)-1] == 'D' or i[len(i)-1] == ')')])
 print(count_smileys([';]', ':[', ';*', ':$', ';-D']))'''
+
+def tickets(people):
+    cashier = {25: 0, 50: 0, 100: 0}
+    for i in range(len(people)):
+        if people[i] == 25:
+            cashier[25] += 1
+        elif people[i] == 50:
+            if cashier[25] >= 1:
+                cashier[25] -= 1
+            else:
+                return "No"
+            cashier[50] += 1
+        elif people[i] == 100:
+            if cashier[25] >= 3:
+                cashier[25] -= 3
+            elif cashier[25] >= 1 and cashier[50] >= 1:
+                cashier[25] -= 1
+                cashier[50] -= 1
+            else:
+                return "No"
+            cashier[100] += 1
+    return "Yes"
+
+print(tickets([25, 25, 50, 50, 100]))
