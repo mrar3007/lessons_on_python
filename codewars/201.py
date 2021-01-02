c51932e002d370a1ebaaed12ba02a458148797fb
Arkadiy Petrov <arkadiy271020@gmail.com>
2021-01-02 17:52:48 +0500

    cdwr task on abc

--- codewars/201.py
+++ codewars/201.py
@@ -1,6 +1,17 @@
-def disemvowel(str):
+'''def disemvowel(str):
     arr = ['a', 'e', 'u', 'i', 'o', 'A', 'E', 'U', 'I', 'O']
     return ''.join([i for i in str if arr.count(i) == 0])
 
-print(disemvowel("This website is for losers LOL!"))
+print(disemvowel("This website is for losers LOL!"))'''
 
+'''def filter_list(l):
+    return [i for i in l if type(i) is int]
+
+print([1, 2, 'a', 'b'])'''
+
+'''def longest(s1, s2):
+    s = sorted(s1 + s2)
+    return ''.join([s[i] for i in range(len(s)) if s[i] != s[i-1]])
+
+print(longest("aretheyhere", "yestheyarehere"))
+'''
