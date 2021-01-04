3aa7c52fd3758598eec64baa0a98f2b37b324427
Arkadiy Petrov <arkadiy271020@gmail.com>
2021-01-04 21:20:45 +0500

    cdwr 8 kyu

--- codewars/401.py
+++ codewars/401.py
@@ -1,8 +1,12 @@
-def printer_error(s):
+'''def printer_error(s):
     arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
     return '{0}/{1}'.format(len(s) - sum([1 for i in s for j in arr if i == j]), len(s))
 
 print(printer_error('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz'))
+'''
 
-
+def remove_char(s):
+    #your code here
+    return s[1:-1]
+print(remove_char('rust'))
 
