f82d1434c2591b98e4f8886229e086d247b39387
Arkadiy Petrov <arkadiy271020@gmail.com>
2021-01-06 22:54:21 +0500

    cdwr 6 kyu

--- codewars/601.py
+++ codewars/601.py
@@ -1,4 +1,9 @@
-def nb_dig(n, d):
+'''def nb_dig(n, d):
 	return sum(str(i*i).count(str(d)) for i in range(n+1))
-print(nb_dig(11011, 2))	
+print(nb_dig(11011, 2))	'''
+
+def presses(phrase):
+    arr = ['', '1ADGJMPTW* #', 'BEHKNQUX0', 'CFILORVY', '23456S8Z', '79']
+    return sum(j for i in phrase.upper() for j in range(len(arr)) if arr[j].count(i) > 0)
+print(presses("HOW R U"))
 
