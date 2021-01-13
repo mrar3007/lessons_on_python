056cc616d34d9ad098ae75dc2442b7c743ea09d1
Arkadiy Petrov <arkadiy271020@gmail.com>
2021-01-13 16:11:35 +0500

    cdwr 5 kyu

--- codewars/1201.py
+++ codewars/1201.py
@@ -1,4 +1,4 @@
-def solution(s, markers):
+'''def solution(s, markers):
     s = [i for i in s]
     res = []
     com = True
@@ -20,10 +20,37 @@ def solution(s, markers):
 print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
 print(solution("a #b\nc\nd $e f g", ["#", "$"]))
 print(solution('cherries\n^ = watermelons\n, bananas lemons watermelons strawberries\navocados oranges lemons', ['-', '=', ',', '!', '^', "'", '#']))
+'''
 
+def anagrams(word, words):
+    arr = []
+    obj = {}
+    obj1 = {}
+    count = 0
+    for i in word:
+        if i in obj:
+            obj[i] += 1
+        else:
+            obj[i] = 1
+    for i in words:
+        for j in i:
+            if j in obj1:
+                obj1[j] += 1
+            else:
+                obj1[j] = 1
+        print(obj1)
+        for j in obj1:
+            if j in obj:
+                if obj1[j] == obj[j]:
+                    count += 1
+            else:
+                break
+        if count == len(obj):
+            arr.append(i)
+        obj1 = {}
+        count = 0
+    return arr
 
-
-
-
+print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))
 
 
