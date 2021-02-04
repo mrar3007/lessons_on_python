3aafeb96ded8854c69f5deeedb59aa08d2c43c12
Arkadiy Petrov <arkadiy271020@gmail.com>
2021-02-05 00:12:59 +0500

    list task 2

--- coursera/solution.py
+++ coursera/solution.py
@@ -1,7 +1,12 @@
-s, n = (int(i) for i in input().split())
-arr = sorted(int(input()) for i in range(n))
-count = 0
-for i in range(len(arr)):
-    if sum(arr[:i+1]) <= s:
-        count += 1
-print(count)
+n = int(input())
+arr = [int(i) for i in input().split()]
+m = int(input())
+mas = [int(i) for i in input().split()]
+result = []
+s = ''
+for i in range(n):
+    for j in range(m):
+        result.append(abs(arr[i]-mas[j]))
+    s += str(result.index(min(result)) + 1) + ' '
+    result.clear()
+print(*[])
